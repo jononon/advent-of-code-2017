357d29acce6816a624d9e9f158cf105d5ebc014a
Jonathan Damico <jj_damico@yahoo.com>
2016-12-05 09:21:45 -0800

    part 1 done

---
+++ Day 5/src/Part1.java
@@ -0,0 +1,33 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
+
+public class Part1 {
+
+	public static void main (String args[]) {
+		String input = "ffykfhsq";
+		MessageDigest md;
+		try {
+			md = MessageDigest.getInstance("MD5");
+		
+			StringBuilder pw = new StringBuilder();
+			
+			for(int i = 0; pw.length()<8; i++) {
+				byte[] hash = md.digest((input+i).getBytes());
+				String hex = (new HexBinaryAdapter()).marshal(hash);
+				if(hex.substring(0, Math.min(hex.length(), 5)).equals("00000")) {
+					System.out.println(hex);
+					System.out.println(hex.charAt(5));
+					pw.append(hex.charAt(5));
+				}
+			}
+			System.out.println(pw.toString().toLowerCase());
+			
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
