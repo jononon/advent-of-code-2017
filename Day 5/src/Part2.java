bc81b9e199942f2f05a5ced1da7607d1b2cea9c8
Jonathan Damico <jj_damico@yahoo.com>
2016-12-05 09:36:49 -0800

    part 2 done + some part 1 cleanup

--- Day 5/src/Part2.java
+++ Day 5/src/Part2.java
@@ -4,23 +4,29 @@ import java.util.Arrays;
 
 import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
 
-public class Part1 {
+public class Part2 {
 
 	public static void main (String args[]) {
 		String input = "ffykfhsq";
 		MessageDigest md;
 		try {
 			md = MessageDigest.getInstance("MD5");
-		
-			StringBuilder pw = new StringBuilder();
 			
-			for(int i = 0; pw.length()<8; i++) {
+			char[] pw = new char[8];
+			for(int i = 0; (new String(pw)).indexOf('\u0000')!=-1; i++) {
 				byte[] hash = md.digest((input+i).getBytes());
 				String hex = (new HexBinaryAdapter()).marshal(hash);
-				if(hex.substring(0, Math.min(hex.length(), 5)).equals("00000"))
-					pw.append(hex.charAt(5));
+				if(hex.substring(0, Math.min(hex.length(), 5)).equals("00000")) {
+					try {
+						int index = Character.getNumericValue(hex.charAt(5));
+						if(pw[index]=='\u0000')
+							pw[index]=hex.charAt(6);
+					} catch (ArrayIndexOutOfBoundsException e) {
+						System.err.println("Out of bounds: "+hex.charAt(5));
+					}
+				}
 			}
-			System.out.println(pw.toString().toLowerCase());
+			System.out.println((new String(pw)).toLowerCase());
 			
 		} catch (NoSuchAlgorithmException e) {
 			// TODO Auto-generated catch block
