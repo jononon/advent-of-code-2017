fd97c5c88c9639944cdc7811fe1d91f588388744
Jonathan Damico <jj_damico@yahoo.com>
2016-12-06 15:54:38 -0800

    done with day 6

---
+++ Day 6/src/Part2.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Part2 {
+	
+	public static void main (String args[]) {
+		ArrayList<char[]> document = readFile("input.in");
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i<document.get(i).length; i++) {
+			int[] popularity = new int[26];
+			
+			for(int j = 0; j<document.size(); j++) 
+				popularity[document.get(j)[i]-97]++;
+			
+			int min = Integer.MAX_VALUE, index = 0;
+			for(int j = 0; j<popularity.length; j++) {
+				if(popularity[j] < min && popularity[j] > 0) {
+					min = popularity[j];
+					index = j;
+				}
+			}
+			sb.append((char)(index+97));
+		}
+		
+		System.out.println(sb);
+	}
+	
+	public static ArrayList<char[]> readFile (String filename) {
+		try {
+			Scanner s = new Scanner(new File(filename));
+			
+			ArrayList<char[]> lines = new ArrayList<char[]>();
+			
+			while(s.hasNextLine())
+				lines.add(s.nextLine().toCharArray());
+			
+			s.close();
+			
+			return lines;
+			
+		} catch (FileNotFoundException e) {
+			System.err.println(e);
+		}
+		return null;
+	}
+}
